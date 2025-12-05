ff41d5cbbc946d3a7de4e6c8bc684a59800b42c8
omarRodFrag <158507789+omarRodFrag@users.noreply.github.com>
2025-12-05 12:26:40 -0600

    update: Preparación Back Host

--- Backend/Directions.py
+++ Backend/Directions.py
@@ -293,4 +293,4 @@ def procesar_venta(current_user):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     logger.info(f"Arrancando servidor en puerto {port}")
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
